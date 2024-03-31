c063f66e2f2f17d15c6df36edd607040f060daa3
nobody's watching <118339799+nobodywatchin@users.noreply.github.com>
2024-03-30 21:54:47 -0700

    Create enable-gnome-extensions.sh

---
+++ config/scripts/enable-gnome-extensions.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Enables custom extensions
+gnome-extensions enable gsconnect
+gnome-extensions enable appindicator
+gnome-extensions enable blur-my-shell
+gnome-extensions enable user-theme
+gnome-extensions enable caffeine
+gnome-extensions enable drive-menu
+gnome-extensions enable forge
+gnome-extensions enable tailscale-gnome-qs
+gnome-extensions enable vitals
