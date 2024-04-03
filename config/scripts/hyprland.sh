9bea3901d657ba08fb6bfeaabefdea1dc3ee5ec2
nobody's watching <118339799+nobodywatchin@users.noreply.github.com>
2024-04-02 22:07:30 -0700

    Update hyprland.sh

--- config/scripts/hyprland.sh
+++ config/scripts/hyprland.sh
@@ -3,6 +3,9 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
+# Activates custom dotfiles
+ags
+
 # Removes cringe anime wallpapers
 for file in /usr/share/hyprland/*.png; do
     if [[ $file == *anime*.png ]]; then
@@ -10,6 +13,3 @@ for file in /usr/share/hyprland/*.png; do
         echo "Removed: $file"
     fi
 done
-
-# Activates custom dotfiles
-# ags
