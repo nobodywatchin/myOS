751ba230c4a77ef44a7e0a4b7fd31a5f15131671
nobody's watching <118339799+nobodywatchin@users.noreply.github.com>
2024-04-02 23:07:38 -0700

    Update hyprland.sh

--- config/scripts/hyprland.sh
+++ config/scripts/hyprland.sh
@@ -14,5 +14,8 @@ for file in /usr/share/hyprland/*.png; do
     fi
 done
 
-# Sets Theming
-sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme 
+# Removes GNOME Desktop Files
+rm /usr/share/applications/org.gnome.ColorProfileViewer.desktop
+rm /usr/share/applications/org.gnome.Settings.desktop
+rm /usr/share/applications/org.gnome.Tecla.desktop
+rm /usr/share/applications/org.gnome.Tour.desktop
