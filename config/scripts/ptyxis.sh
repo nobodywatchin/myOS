5e6a0495422e77cc1a6c5256e979e6c844958432
nobody's watching <118339799+nobodywatchin@users.noreply.github.com>
2024-04-22 19:42:52 -0700

    Update ptyxis.sh

--- config/scripts/ptyxis.sh
+++ config/scripts/ptyxis.sh
@@ -3,11 +3,10 @@
 set -oue pipefail
 
 # Add Staging repo
-wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-"${FEDORA_MAJOR_VERSION}"/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo -O /etc/yum.repos.d/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo
+wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-39/ublue-os-staging-fedora-39.repo -O /etc/yum.repos.d/ublue-os-staging-fedora-39.repo
 
 # 39 gets VRR and Ptyxis
-if [ "${FEDORA_MAJOR_VERSION}" -eq "39" ]; then
-    wget https://copr.fedorainfracloud.org/coprs/kylegospo/gnome-vrr/repo/fedora-"${FEDORA_MAJOR_VERSION}"/kylegospo-gnome-vrr-fedora-"${FEDORA_MAJOR_VERSION}".repo -O /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo
+    wget https://copr.fedorainfracloud.org/coprs/kylegospo/gnome-vrr/repo/fedora-39/kylegospo-gnome-vrr-fedora-39.repo -O /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo
     rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter mutter-common gnome-control-center gnome-control-center-filesystem
     rm -f /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo
     rpm-ostree override replace \
@@ -20,21 +19,5 @@ if [ "${FEDORA_MAJOR_VERSION}" -eq "39" ]; then
     rpm-ostree install ptyxis
 fi
 
-# 40 gets Ptyxis and patched Mutter
-if [ "${FEDORA_MAJOR_VERSION}" -eq "40" ]; then
-    rpm-ostree override replace \
-    --experimental \
-    --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
-        vte291 \
-        vte-profile
-    rpm-ostree install ptyxis
-    if [[ "${BASE_IMAGE_NAME}" =~ "silverblue" ]]; then
-        rpm-ostree override replace \
-        --experimental \
-        --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
-            mutter
-    fi
-fi
-
 # Add Nerd Fonts
-wget https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${FEDORA_MAJOR_VERSION}"/che-nerd-fonts-fedora-"${FEDORA_MAJOR_VERSION}".repo -O /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA_MAJOR_VERSION}".repo
+wget https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-39/che-nerd-fonts-fedora-39.repo -O /etc/yum.repos.d/_copr_che-nerd-fonts-39.repo
