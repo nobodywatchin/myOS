0bc420f1c399bae918f6e9ff0e05b4062994f7ef
nobody's watching <118339799+nobodywatchin@users.noreply.github.com>
2024-04-23 13:45:01 -0700

    fix ptyxis

--- config/scripts/initramfs.sh
+++ config/scripts/initramfs.sh
@@ -5,5 +5,3 @@ set -euo pipefail
 rpm-ostree cliwrap install-to-root /
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "${QUALIFIED_KERNEL}" --reproducible -v --add ostree -f "/lib/modules/${QUALIFIED_KERNEL}/initramfs.img"
-
-rpm-ostree initramfs --disable
