96ded8426a9c75dc5d08d76ffd241c840bdf5e29
nobody's watching <118339799+nobodywatchin@users.noreply.github.com>
2024-04-22 20:18:45 -0700

    Update initramfs.sh

--- config/scripts/initramfs.sh
+++ config/scripts/initramfs.sh
@@ -2,5 +2,7 @@
 
 set -oue pipefail
 
+KERNEL_SUFFIX=""
+
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
