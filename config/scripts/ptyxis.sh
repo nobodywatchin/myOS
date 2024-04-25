23ce0617831b7b88590af9d5af34ebdbb5d683bb
nobody's watching <118339799+nobodywatchin@users.noreply.github.com>
2024-04-24 22:10:15 -0700

    Update ptyxis.sh

--- config/scripts/ptyxis.sh
+++ config/scripts/ptyxis.sh
@@ -3,9 +3,10 @@
 set -oue pipefail
 
 # Add Staging repo
-wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-39/ublue-os-staging-fedora-39.repo -O /etc/yum.repos.d/ublue-os-staging-fedora-39.repo
+wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-"${FEDORA_MAJOR_VERSION}"/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo -O /etc/yum.repos.d/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo
 
-rpm-ostree override replace \
+# 40 gets Ptyxis and patched Mutter
+    rpm-ostree override replace \
     --experimental \
     --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
         vte291 \
@@ -15,3 +16,6 @@ rpm-ostree override replace \
         --experimental \
         --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
             mutter
+            
+# Add Nerd Fonts
+wget https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${FEDORA_MAJOR_VERSION}"/che-nerd-fonts-fedora-"${FEDORA_MAJOR_VERSION}".repo -O /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA_MAJOR_VERSION}".repo
